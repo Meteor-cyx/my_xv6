22a28f7dd53c7d6204fae77445d5a6258ca30e80
Ethan Chen <cyx13967018597@163.com>
2025-12-17 11:04:29 +0800

    nothing special

--- kernel/proc.c
+++ kernel/proc.c
@@ -233,6 +233,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
   uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
+
 }
 
 // a user program that calls exec("/init")
